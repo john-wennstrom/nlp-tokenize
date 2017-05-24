4c3394f545b31af3e4ae4a83d5f9b1aac8589e30
Johnny Human <johnny.human@protonmail.ch>
2017-05-24 20:35:46 +0200

    add parser & byte to string conversion

--- README.md
+++ README.md
@@ -1,9 +1,39 @@
 # nlp-tokenize
-It opens a text file, turns it into a byte (u8) vector and tokenizes it by spaces and line breaks.
+It tokenizes a text file by spaces and line breaks.
+It then categorizes the tokens using bitflags.
+The token has this format:
+```
+struct Token {
+   index: usize,
+   flags: Flags,
+   data: String
+}
+```
 
-It then categorizes the tokens using HasSets.
+Example output:
+```
+Token { index: 358158, flags: ALPHA, 			data: "TIME" }
+Token { index: 358159, flags: ALPHA, 			data: "From" }
+Token { index: 358160, flags: ALPHA, 			data: "Amit" }
+Token { index: 358161, flags: ALPHA | STOP | BRACKET, 	data: "(Sanskrit)," }
+Token { index: 358162, flags: ALPHA, 			data: "which" }
+Token { index: 358163, flags: ALPHA, 			data: "means" }
+Token { index: 358164, flags: ALPHA, 			data: "endless" }
+Token { index: 358165, flags: ALPHA, 			data: "or" }
+Token { index: 358166, flags: ALPHA | FULL_STOP, 	data: "immeasurable." }
+```
 
-Benchmark has results of 2Mb/s, or 338000 tokens/s. If someone knows how to increase performance, I value your feedback. =)
+Benchmark 1:
+```
+Using HashSet for flags
+2Mb/s	338000 tokens/s
+```
+
+Benchmark 2:
+```
+Using Bitflags
+446600 tokens/s
+```
 
 Run it like this:
 `cargo run -- <FILE>`
--- src/tokenizer_loop.rs
+++ src/tokenizer_loop.rs
@@ -1,20 +1,26 @@
 use bitflags::*;
+use std::borrow::Cow;
 
 #[derive(Debug, PartialEq)]
 pub struct Token {
       index: usize,
       flags: Flags,
-      data: Vec<u8>
+      data: String //Vec<u8>
 }
 
 bitflags! {
       #[derive(Default)]
-      pub struct Flags: u32 {
-            const CONTROL     = 0b00000001;
-            const SPECIAL     = 0b00000010;
-            const NUMBER      = 0b00000100;
-            const ALPHA       = 0b00001000;
-            const LATIN       = 0b00010000;
+      pub struct Flags: u64 {
+            const CONTROL     = 0b0000000000000001;
+            const SPECIAL     = 0b0000000000000010;
+            const NUMBER      = 0b0000000000000100;
+            const ALPHA       = 0b0000000000001000;
+            const LATIN       = 0b0000000000010000;
+            const STOP        = 0b0000000000100000;
+            const FULL_STOP   = 0b0000000001000000;
+            const QUOTE       = 0b0000000010000000;
+            const BRACKET     = 0b0000000100000000;
+            const HYPHEN      = 0b0000001000000000;
       }
 }
 
@@ -34,63 +40,120 @@ pub fn words(bytes: Vec<u8>) -> Result<Vec<Token>, &'static Vec<u8>> {
       
       for byte in bytes {
             
+            // Increase buffer if needed
             if buffer.len() == MAX_BYTES { 
                   buffer.reserve(MAX_BYTES); 
             };
                         
             match byte {
-                  32u8 | 10u8 | 13u8 => {
+                  32 | 10 | 13 => {
                         if buffer.len() == 0 { continue; };
                         i += 1;
                         
                         let token = Token {
                               index: i,
                               flags: parse_flags,
-                              data: buffer.clone()
+                              data: as_string(buffer.clone())
                         };
                         println!("{:?}", token );
                         
                         tokens.push( token );
                         buffer.clear();
                         parse_flags.clear();
-                        
-                  },
-                  1 ... 31    => { parse_flags = parse_flags | CONTROL;   buffer.push(byte);},
-                  32 ... 47   => { parse_flags = parse_flags | SPECIAL;   buffer.push(byte);},
-                  48 ... 57   => { parse_flags = parse_flags | NUMBER;    buffer.push(byte);},
-                  58 ... 64   => { parse_flags = parse_flags | SPECIAL;   buffer.push(byte);},
-                  65 ... 90   => { parse_flags = parse_flags | ALPHA;     buffer.push(byte);},
-                  91 ... 96   => { parse_flags = parse_flags | SPECIAL;   buffer.push(byte);},
-                  97 ... 122  => { parse_flags = parse_flags | ALPHA;     buffer.push(byte);},
-                  123 ... 126 => { parse_flags = parse_flags | SPECIAL;   buffer.push(byte);},
-                  127 ... 159 => { parse_flags = parse_flags | CONTROL;   buffer.push(byte);},
-                  160 ... 191 => { parse_flags = parse_flags | SPECIAL;   buffer.push(byte);},
-                  192 ... 255 => { parse_flags = parse_flags | LATIN;     buffer.push(byte);},
-                  _           => { 
-                        buffer.push(byte);
-                  }
+                        },
+                  65 ... 90 | 97 ... 122 => { 
+                        buffer.push(byte); 
+                        parse_flags = parse_flags | ALPHA;
+                        },
+                  33 ... 47 | 58 ... 64 | 91 ... 96 | 123 ... 126 | 160 ... 191 => { 
+                        buffer.push(byte); 
+                        parse_flags = parse_flags | parse_special(byte);
+                        },
+                  48 ... 57 => { 
+                        buffer.push(byte); 
+                        parse_flags = parse_flags | NUMBER;
+                        },
+                  127 ... 159 | 1 ... 31 => { 
+                        buffer.push(byte); 
+                        parse_flags = parse_flags | CONTROL;
+                        },
+                  192 ... 255 => { 
+                        buffer.push(byte); 
+                        parse_flags = parse_flags | LATIN;
+                        },
+                  _ => { 
+                        buffer.push(byte); 
+                        }
             }
-  
-            /* Handle UTF-8 block 128-255 Basic Latin
-            else {
-                  let le = if byte < 192 {194} else {195};
-                  let be = if byte < 192 {byte} else {byte - 64};
-                  
-                  buffer.push(le);
-                  buffer.push(be);       
-            }*/
       }
       
-      /*if buffer.len() > 0 { 
+      // Get the last token from buffer
+      if buffer.len() > 0 { 
             let token = Token {
-                  data: buffer.clone(),
-                  length: buffer.len(),
-                  index: i
+                  index: i + 1,
+                  flags: parse_flags,
+                  data: as_string(buffer.clone())
             };
-            tokens.push( token );
-            //println!("{:?}", String::from_utf8(buffer).unwrap());
-      }*/
-      
+            println!("{:?}", token );
+      }
       Ok( tokens )
 }
 
+/*
+ * Parse Special characters and return Flags
+ *
+ */
+pub fn parse_special(byte: u8) -> Flags {
+      match byte {
+            44 | 58 | 59 => { 
+                  STOP 
+                  },
+            45 => {
+                  HYPHEN
+                  },
+            33 | 46 | 63 => { 
+                  FULL_STOP 
+                  },
+            34 => { 
+                  QUOTE 
+                  },
+            40 | 41 | 60 | 62 | 91 | 93 | 123 | 125 => { 
+                  BRACKET 
+                  },
+            _ => {
+                  SPECIAL
+                  }
+      }
+}
+
+/*
+ * Transform a code point in the range U+0080-U+00FF into decoded UTF-8
+ * U80_FF( 209 ) -> [195, 145]
+ *
+ * u8 slice must know the size statically, else we have to use lifetimes
+ */
+pub fn U80_FF(byte: u8) -> [u8; 2] {
+      let slice = if byte < 192 {[194u8, byte]} else {[195u8, byte - 64u8]};
+      slice
+}
+
+/*
+ * Transform a vector of bytes into a String
+ *
+ */
+pub fn as_string(input: Vec<u8>) -> String {
+      let mut buf: Vec<u8> = Vec::with_capacity(input.len() * 2);
+      
+      for byte in input {
+            if byte > 127 {
+                  buf.extend_from_slice( &U80_FF(byte).to_owned() );
+            } else {
+                  buf.push(byte);
+            }
+      }
+      let result = unsafe {
+            String::from_utf8_unchecked(buf)
+      };
+      
+      result
+}
